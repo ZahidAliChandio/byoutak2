9c63131b30df77d4f0e659844f20670f5b357339
ZahidAliChandio <zahidalichandio240@gmail.com>
2022-12-19 18:37:04 +0500

    sidebar updated.

--- src/places/components/Navigation/NavLinks.js
+++ src/places/components/Navigation/NavLinks.js
@@ -40,12 +40,15 @@ const NavLinks = (props) => {
   ];
 
   return (
-    <ul className="flex flex-col gap-4 text-gray-300 my-4 font-semibold w-full">
+    <ul className="flex flex-col gap-1 text-gray-300 my-4 font-semibold w-full">
       {lis.map((content, index) => (
         <li key={index} className="" ref={nodeRef}>
           <NavLink
             to={content.linkTo}
-            className="flex items-center gap-4 text-gray-300 text-xs w-[200px] pl-4 hover:text-[color:var(--red-color)]"
+            // className="flex items-center gap-4 text-gray-300 text-xs w-[200px] pl-4 hover:text-[color:var(--red-color)]"
+            className={`flex items-center gap-4 w-[230px]                 
+                  border-[3px] border-transparent  text-xs bg-transparent py-[10px] cursor-pointer font-gillsans px-3 text-white 
+                  hover:border-l-[color:var(--red-color)] hover:text-[color:var(--red-color)]  hover:bg-[#070707] hover:pl-[0.88rem]`}
           >
             <div className="">
               <i className="fas fa-chevron-right w-1"></i>
